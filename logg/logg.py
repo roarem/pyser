f965eeea0ce8dd7de15c79f592f88b530f2f26df
A Non <anon@anonymous.ano>
2017-01-15 10:12:40 +0100

    46346

--- logg/logg.py
+++ logg/logg.py
@@ -1,4 +1,6 @@
 import time, textwrap
+import curses as cu
+import curses.textpad as cut
 
 def logger(logg_input):
     date = time.strftime('%A %d %b %H:%M:%S %Y')
@@ -13,7 +15,7 @@ def get_input():
     print('Start writing, please')
     while 1:
         pre_fetch = input()
-        if pre_fetch =="doneexit":
+        if pre_fetch =="exit":
             print('thank you for your contribution, sucker...')
             break
         else:
@@ -23,8 +25,27 @@ def get_input():
     print(wrapped)
     return wrapped
 
+def get_input_cu():
+    stdscr = cu.initscr()
+    stdscr.addstr(0, 0, "Log (Ctrl-G to end)")
+
+    editwin = cu.newwin(8,100, 2,1)
+    cut.rectangle(stdscr, 1,0, 1+8+1, 1+100+1)
+    stdscr.refresh()
+
+    box = cut.Textbox(editwin)
+
+    box.edit()
+
+    message = box.gather()
+    cu.endwin()
+
+    wrapped = textwrap.wrap(message,width=100)
+    logger(wrapped)
+
 if __name__=='__main__':
-    fetched = 99*'4'+' '+1*'2'
-    fetched = textwrap.wrap(fetched,width=100)
+    #fetched = 99*'4'+' '+1*'2'
+    #fetched = textwrap.wrap(fetched,width=100)
     #fetched = get_input()
-    logger(fetched)
+    get_input_cu()
+    #logger(fetched)
